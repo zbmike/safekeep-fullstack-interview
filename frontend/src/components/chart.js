85f82e0fe13d62292f970ef031fb29b7a5e058a4
Zhiyuan He <zh2312@columbia.edu>
2021-06-19 11:26:39 -0400

    Add donut chart component

---
+++ frontend/src/components/chart.js
@@ -0,0 +1,40 @@
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import { useLayoutEffect, useRef } from "react";
+
+am4core.useTheme(am4themes_animated);
+
+function Chart({ data }) {
+  const chart = useRef(null);
+
+  useLayoutEffect(() => {
+    let x = am4core.create("chartdiv", am4charts.PieChart3D);
+    x.hiddenState.properties.opacity = 0; // this creates initial fade-in
+
+    x.data = [];
+
+    x.innerRadius = 60;
+
+    let series = x.series.push(new am4charts.PieSeries3D());
+    series.dataFields.value = "hours";
+    series.dataFields.category = "name";
+
+    chart.current = x;
+    return () => {
+      x.dispose();
+    };
+  });
+  useLayoutEffect(() => {
+    if (data) {
+      let chartData;
+      if (data) chartData = data.map(dat => ({name: `${dat.name} ${dat.surName}`, hours: dat.hours}));
+
+      chart.current.data = chartData;
+    }
+  }, [data]);
+
+  return <div id="chartdiv" style={{ width: "100%", height: "250px" }}></div>;
+}
+
+export default Chart;
