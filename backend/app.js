ea0344e02e9ab903dc7eef80cc3d769b7f7dff96
Zhiyuan He <zh2312@columbia.edu>
2021-06-18 21:26:43 -0400

    Implement simple model

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,18 @@ const sequelize = new Sequelize(
   `postgres://trust_our_leader:${sqlPassword}@host.docker.internal:5432/postgres`
 );
 
-
-
-
+const Participant = sequelize.define('participant', {
+    id: {
+        field: 'ParticipantId',
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    name: {
+        field: 'Name',
+        type: Sequelize.STRING,
+    },
+    surname: {
+        field: 'Surname',
+        type: Sequelize.STRING,
+    },
+});
